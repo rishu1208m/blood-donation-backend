f0f86b98eb82a1b6d45483be70ec5600fb432fc7
Your Name <your@email.com>
2026-04-01 07:11:20 +0530

    update the api form url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://blood-donation-backend-bzb8.onrender.com/api"
+    baseURL: "https://blood-donation-backend-bzb8.onrender.com"
 });
 
 API.interceptors.request.use((req) => {
